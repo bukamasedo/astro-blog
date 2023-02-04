802225a8a32ce4c987dfe0842d604574d5679e34
廣内秀有 <>
2023-02-05 01:05:51 +0900

    add: creation blog site

---
+++ src/datas/skills.ts
@@ -0,0 +1,64 @@
+export const frontSkills = [
+  {
+    name: 'JavaScript',
+    href: 'https://developer.mozilla.org/ja/docs/Web/JavaScript',
+  },
+  {
+    name: 'TypeScript',
+    href: 'https://www.typescriptlang.org/docs/',
+  },
+  {
+    name: 'jQuery',
+    href: 'https://jquery.com/',
+  },
+  {
+    name: 'React',
+    href: 'https://ja.reactjs.org/docs/getting-started.html',
+  },
+  {
+    name: 'Next.js',
+    href: 'https://nextjs.org/',
+  },
+  {
+    name: 'Vue.js',
+    href: 'https://ja.vuejs.org/',
+  },
+  {
+    name: 'Astro',
+    href: 'https://astro.build/',
+  },
+]
+
+export const backSkills = [
+  {
+    name: 'PHP',
+    href: 'https://www.php.net/manual/ja/index.php',
+  },
+  {
+    name: 'Laravel',
+    href: 'https://laravel.com/docs/9.x',
+  },
+]
+
+export const otherSkills = [
+  {
+    name: 'GitHub',
+    href: 'https://github.co.jp/',
+  },
+  {
+    name: 'GitLab',
+    href: 'https://about.gitlab.com/ja-jp/',
+  },
+  {
+    name: 'neovim',
+    href: 'https://neovim.io/',
+  },
+  {
+    name: 'Firebase',
+    href: 'https://firebase.google.com/?hl=ja',
+  },
+  {
+    name: 'Docker',
+    href: 'https://www.docker.com/',
+  },
+]
